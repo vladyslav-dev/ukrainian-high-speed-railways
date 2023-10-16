b16da0ae4e0926c5c8fac492a815dbbfb0fda7f0
ShapovalDanylo <shapovaldanil2019@gmail.com>
2023-10-16 14:22:52 +0300

    Connected the database to the project

---
+++ server/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UHR.Models;
+
+namespace UHR.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { 
+            
+        }
+
+        public DbSet<Cargo> Cargos { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Passenger> Passengers { get; set; }
+        public DbSet<UHR.Models.Route> Routes { get; set; }
+        public DbSet<Seat> Seats { get; set; }
+        public DbSet<Ticket> Ticketes { get; set; }
+        public DbSet<Train> Traines { get; set; }
+        public DbSet<TrainType> TrainTypes { get; set; }
+        public DbSet<Wagon> Wagones { get; set; }
+        public DbSet<WagonType> WagonTypes { get; set; }
+    }
+}
--- server/Program.cs
+++ server/Program.cs
@@ -1,15 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using UHR.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
