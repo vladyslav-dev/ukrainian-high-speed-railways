5e8592487c592c92eaf450c5302b2b308cdfaa71
vladyslav-dev <grigovlad09112002@gmail.com>
2023-11-28 20:32:29 +0200

    Client: Wagons and Seats sorting

--- client/app/workflow/seats/page.tsx
+++ client/app/workflow/seats/page.tsx
@@ -3,11 +3,11 @@
 import { getWagonsAndSeatsByTripId } from '@/api/trips'
 import Button from '@/components/Button'
 import { useWorkflowStore } from '@/stores/useWorkflowStore'
-import { IWagonsAndSeatsResponse } from '@/types/wagon'
+import { IWagonsAndSeatsResponse, IWagonsResponse } from '@/types/wagon'
 import SelectSeats from '@/widgets/SelectSeats'
 import Image from 'next/image'
 import { useRouter, useSearchParams } from 'next/navigation'
-import React from 'react'
+import React, { useMemo } from 'react'
 import useSWR from 'swr'
 import { Lexend_Giga } from 'next/font/google'
 import Link from 'next/link'
@@ -33,6 +33,28 @@ export default function Seats() {
       isLoading: isWagonsLoading
   } = useSWR<IWagonsAndSeatsResponse>(`/api/Trip/GetWagonsAndSeatsByTripId/${backTripId || tripId}`, tripId ? () => getWagonsAndSeatsByTripId(backTripId || tripId) : null)
 
+  const sortedWagons = useMemo<IWagonsAndSeatsResponse | null>(() => {
+    if (!wagons?.tripWagons) return null
+
+    const sortedSeats = wagons.tripWagons.map((wagon: IWagonsResponse) => {
+      return {
+        ...wagon,
+        seats: wagon.wagonSeats.sort((a, b) => {
+          return a.number - b.number
+        })
+      }
+    })
+
+    const sortedTripWagons = sortedSeats.toSorted((a, b) => {
+      return a.wagonNumber - b.wagonNumber
+    })
+
+    return {
+      tripId: wagons?.tripId,
+      tripWagons: sortedTripWagons
+    }
+  }, [wagons?.tripWagons])
+
   const onBackClick = () => {
     router.back()
   }
@@ -57,16 +79,16 @@ export default function Seats() {
         </div>
     )
   } else {
-    return wagons && activeTrip && trip ? (
+    return sortedWagons && activeTrip && trip ? (
       <React.Fragment>
-        <SelectSeats trip={trip} wagons={wagons} title={title} />
+        <SelectSeats trip={trip} wagons={sortedWagons} title={title} />
         <div className='h-[90px] p-4 flex justify-end items-center border-t-2 border-stroke'>
           <Button label='Back' onClick={onBackClick} size='medium' variant='outlined' />
           <Button disabled={!Boolean(selectedSeats.length)} label='Next' onClick={onNextClick} size='medium' className='ml-4' />
         </div>
       </React.Fragment>
-    ) 
-    : 
+    )
+    :
     <div className='h-3/4 flex flex-col gap-5 justify-center items-center'>
         <Image priority src={"/went-wrong.jpg"} alt="Went wrong" width={695} height={252}/>
         <h1 className={lexendGigaFont.className}>Something went wrong, try to find the trip again!</h1>
