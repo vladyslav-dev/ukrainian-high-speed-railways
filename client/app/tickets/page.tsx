0fb3a5b2452426e2098202abdcb8b9a626d5e16a
vladyslav-dev <grigovlad09112002@gmail.com>
2023-11-29 00:50:48 +0200

    Client: No Tickets found

--- client/app/tickets/page.tsx
+++ client/app/tickets/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { getTicketsByPhone } from "@/api/tickets"
+import NotFound from "@/components/NotFound"
 import { TTicketsByPhoneResponse } from "@/types/ticket"
 import { useEffect, useState } from "react"
 
@@ -24,31 +25,38 @@ const TicketsPage = () => {
     return (
         <div className="h-full w-full bg-white p-4 rounded-tl-[6px] rounded-tr-[6px] overflow-hidden">
             <h1 className="text-2xl p-4 mb-4">Tickets</h1>
-            <div className="h-full overflow-auto py-8">
-                <div className="mx-auto">
-                    <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-                        {tickets.map((ticket, index) => (
-                            <div key={index} className="bg-white rounded-lg overflow-hidden shadow-md last:mb-16">
-                                <div className="p-6">
-                                    <h2 className="text-xl font-bold mb-2">{ticket.tripName}</h2>
-                                    <p className="text-sm mb-2">
-                                    From: {ticket.cityFrom} - To: {ticket.cityTo}
-                                    </p>
-                                    <p className="text-sm mb-2">Departure: {new Date(ticket.departureDate).toLocaleString()}</p>
-                                    <p className="text-sm mb-2">Arrival: {new Date(ticket.arrivalDate).toLocaleString()}</p>
-                                    <p className="text-sm mb-2">Seat Number: {ticket.seatNumber}</p>
-                                    <p className="text-sm mb-2">
-                                    Passenger: {ticket.passengerName} {ticket.passengerMiddleName} {ticket.passengerLastName}
-                                    </p>
-                                    <p className="text-sm mb-2">Email: {ticket.passengerEmail}</p>
-                                    <p className="text-sm mb-2">Phone: {ticket.passengerPhone}</p>
-                                    <p className="text-sm mb-2">Price: ${ticket.wagonPrice}</p>
+            {tickets.length === 0 ? (
+                <NotFound
+                    title="No tickets found"
+                    description="You have not bought any tickets yet."
+                />
+            ) : (
+                <div className="h-full overflow-auto py-8">
+                    <div className="mx-auto">
+                        <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+                            {tickets.map((ticket, index) => (
+                                <div key={index} className="bg-white rounded-lg overflow-hidden shadow-md last:mb-16">
+                                    <div className="p-6">
+                                        <h2 className="text-xl font-bold mb-2">{ticket.tripName}</h2>
+                                        <p className="text-sm mb-2">
+                                        From: {ticket.cityFrom} - To: {ticket.cityTo}
+                                        </p>
+                                        <p className="text-sm mb-2">Departure: {new Date(ticket.departureDate).toLocaleString()}</p>
+                                        <p className="text-sm mb-2">Arrival: {new Date(ticket.arrivalDate).toLocaleString()}</p>
+                                        <p className="text-sm mb-2">Seat Number: {ticket.seatNumber}</p>
+                                        <p className="text-sm mb-2">
+                                        Passenger: {ticket.passengerName} {ticket.passengerMiddleName} {ticket.passengerLastName}
+                                        </p>
+                                        <p className="text-sm mb-2">Email: {ticket.passengerEmail}</p>
+                                        <p className="text-sm mb-2">Phone: {ticket.passengerPhone}</p>
+                                        <p className="text-sm mb-2">Price: ${ticket.wagonPrice}</p>
+                                    </div>
                                 </div>
-                            </div>
-                        ))}
+                            ))}
+                        </div>
                     </div>
                 </div>
-            </div>
+            )}
         </div>
     )
 }
